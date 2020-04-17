ee942a743660abbf228831e5b3e6cb13cfafceea
Kirill Krupina <kirill.krupina26@gmail.com>
2020-04-17 15:28:13 +0300

    init

---
+++ error.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Error 404, go to <a href="/index.php">main page</a>
+</body>
+</html>
