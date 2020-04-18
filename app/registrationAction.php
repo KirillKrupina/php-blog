3214b0cfc2a0026e0411af9bbf9ee0e863219abf
Kirill Krupina <kirill.krupina26@gmail.com>
2020-04-18 12:58:54 +0300

    AJAX requesting

--- app/registrationAction.php
+++ app/registrationAction.php
@@ -3,23 +3,39 @@ $login = trim(filter_var($_POST['login'], FILTER_SANITIZE_STRING));
 $email = trim(filter_var($_POST['email'], FILTER_SANITIZE_EMAIL));
 $password = trim(filter_var($_POST['password'], FILTER_SANITIZE_STRING));
 
-if ($login == "" && strlen($login) <= 3) {
-    exit();
+$error = '';
+if ($login == "") {
+    $error = 'Enter your login';
+} else if (strlen($login) <= 4) {
+    $error = 'Your login should be more than 4 symbols';
 } else if ($email == "") {
-    exit();
-} else if ($password == "" && strlen($password) <= 3) {
-    exit();
+    $error = 'Enter your email correctly';
+} else if ($password == "") {
+    $error = 'Enter your password';
+} else if (strlen($password) <= 6) {
+    $error = 'Your password should be more than 6 symbols';
 }
 
-$password = md5($password);
+if ($error != '') {
+    exit();
+} else {
+    try {
+        $password = md5($password);
 
-$db_user = 'root';
-$db_password = '';
-$db = "php_web_blog";
-$host = "127.0.0.1";
+        $db_user = 'root';
+        $db_password = '';
+        $db = "php_web_blog";
+        $host = "127.0.0.1";
 
-$dsn = 'mysql:host='.$host.';dbname='.$db;
-$pdo = new PDO($dsn, $db_user, $db_password);
-$sql = 'INSERT INTO users(login, email, password) VALUES(?, ?, ?)';
-$query = $pdo->prepare($sql);
-$query -> execute([$login, $email, $password]);
+        $dsn = 'mysql:host=' . $host . ';dbname=' . $db;
+        $pdo = new PDO($dsn, $db_user, $db_password);
+        $sql = 'INSERT INTO users(login, email, password) VALUES(?, ?, ?)';
+        $query = $pdo->prepare($sql);
+        $query->execute([$login, $email, $password]);
+
+        echo 'Success';
+    } catch (Exception $e) {
+        echo 'Error: #' . $e;
+    }
+}
+
--- templates/registration.php
+++ templates/registration.php
@@ -4,28 +4,75 @@
 <head>
     <?
     $title = 'Registration';
-    require 'blocks/head.php'
+    require 'blocks/head.php';
     ?>
 </head>
 
 <body class="">
-    <? require 'blocks/header.php' ?>
+<? require 'blocks/header.php' ?>
 
 
-    <form action="/app/registrationAction.php" method="post" class="form-signin" style=" max-width: 400px; margin: auto;">
-        <h2 class="h3 mb-3 font-weight-normal" style="text-align: center">Please sign up</h2>
+<form action="" method="" class="form-signin" style=" max-width: 400px; margin: auto;">
+    <h2 class="h3 mb-3 font-weight-normal" style="text-align: center">Sign up</h2>
 
-        <label for="login" class="">Login:</label>
-        <input name="login" type="text" id="login" class="form-control mb-2" placeholder="Login" required="" autofocus="">
-        
-        <label for="inputEmail" class="">Email address:</label>
-        <input name="email" type="email" id="inputEmail" class="form-control mb-2" placeholder="Email address" required="" autofocus="">
+    <div class="alert alert-danger mb-2" id="errorBlock">
+    </div>
 
-        <label for="inputPassword" class="">Password:</label>
-        <input name="password" type="password" id="inputPassword" class="form-control mb-4" placeholder="Password" required="">
-        
-        <button class="btn btn-lg btn-primary btn-block mb-3" type="submit">Sign up</button>
-    </form>
+    <div class="alert alert-success mb-2" id="successBlock">
+    </div>
+
+    <label for="login" class="">Login:</label>
+    <input name="login" type="text" id="login" class="form-control mb-2" placeholder="Login" required="" autofocus="">
+
+    <label for="inputEmail" class="">Email address:</label>
+    <input name="email" type="email" id="email" class="form-control mb-2" placeholder="Email address" required=""
+           autofocus="">
+
+    <label for="inputPassword" class="">Password:</label>
+    <input name="password" type="password" id="password" class="form-control mb-4" placeholder="Password" required="">
+
+    <button class="btn btn-lg btn-primary btn-block mb-3" type="button" id="reg_user">Sign up</button>
+</form>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+<script>
+    $('#reg_user').click(() => {
+        let login = $('#login').val();
+        let email = $('#email').val();
+        let password = $('#password').val();
+
+        $.ajax({
+            url: '/app/registrationAction.php',
+            type: 'POST',
+            data: {
+                'login': login,
+                'email': email,
+                'password': password
+            },
+            dataType: 'html',
+            success: function (data) {
+                if (data === 'Success') {
+                    let successBlock = $('#successBlock');
+                    successBlock.show();
+                    successBlock.text(data);
+                    setTimeout(() => {
+                        window.location = '/';
+                    }, 1500)
+
+                } else {
+                    let errorBlock = $('#errorBlock');
+                    errorBlock.show();
+                    errorBlock.text(data);
+                    setTimeout(() => {
+                        errorBlock.hide();
+                    }, 3000)
+                }
+            }
+        });
+    })
+</script>
 
 </body>
 
